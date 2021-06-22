198682217d6f8f21cc50a234f5b4914871f83f3a
1NikolayDmitriev1 <66631047+1NikolayDmitriev1@users.noreply.github.com>
2021-06-22 12:37:33 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const mongoose = require("mongoose");
 require("dotenv").config();
+const { MDKey } = require("./config/keys");
 const app = express();
 app.use(express.json({ extended: true }));
 app.use("/api/auth", require("./routes/auth.routes"));
 app.use("/api/app", require("./routes/app.routes"));
 async function start() {
   try {
-    await mongoose.connect(process.env.DB_KEY, {
+    await mongoose.connect(MDKey, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useCreateIndex: true,
